ee464a60db51806cf5e3cc78d19c8ffc51240a4f
ocean <osciris.veliz@gmail.com>
2022-09-25 15:33:33 -0500

    add more formatting to get date in DD/MM/YYYY with formatted time stamp

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,8 +1,11 @@
 const formatDate = date => {
-  console.log('date timestamp:', date);
+  console.log('date timestamp as is:', date);
 
   let dateString = date.toString();
-  console.log('new date string:', dateString)
+  console.log('new date string:', dateString);
+
+  return `${new Date(dateString).getMonth() + 1}/${new Date(dateString).getDate()}/${new Date(
+    dateString).getFullYear()} @ ${new Date(dateString).toLocaleTimeString()}`
 }
 
 module.exports = formatDate;
